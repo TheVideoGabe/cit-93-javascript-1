56a7794dfa0f2bbcb4f8c929ab9c1c98a7b0ef43
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-01-13 13:56:30 -0800

    video 10 my code

---
+++ js-bootcamp/basics/strings.js
@@ -0,0 +1,19 @@
+let city = 'Fresno'
+
+let country = 'United States'
+
+let location = city + ', ' + country
+
+console.log(location)
+
+// let firstName = 'Gabriel'
+
+// let lastName = 'Corral'
+
+// let fullName = firstName + ' ' + lastName
+
+// console.log(fullName)
+
+// let name = 'Gabriel Corral'
+
+// console.log(name)
