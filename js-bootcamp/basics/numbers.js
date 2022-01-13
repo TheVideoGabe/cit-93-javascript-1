c1ffbb052752db67d9d78e6e22acaac1863bd165
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-01-13 14:14:00 -0800

    Video 11 my code

---
+++ js-bootcamp/basics/numbers.js
@@ -0,0 +1,15 @@
+let studentScore = 18
+let maxScore = 20
+let percentage = (studentScore / maxScore) * 100
+
+console.log(percentage)
+
+// let age = 23
+// let dogYears = (age + 1) / 7
+
+// console.log(dogYears)
+
+// let x = 11
+// let num = (x + 1) * 2
+
+// console.log(num)
