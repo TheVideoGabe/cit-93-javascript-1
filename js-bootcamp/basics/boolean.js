d8a604cd22c2576355fba8e4a8b4db5ac2f81e78
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-01-13 14:51:51 -0800

    video 14 my code

---
+++ js-bootcamp/basics/boolean.js
@@ -0,0 +1,19 @@
+// === is the equality operator
+// !== this is the not equal operator
+// < is the less than operator
+// > is the grater than opertator
+// <= less than or equal to operator
+// >= grater than or equal to operator
+
+
+// let temp = 31
+// let isFreezing = temp < 32
+
+// console.log(isFreezing)
+
+let age = 23
+let child = age < 7
+let senior = age > 65
+
+console.log(child)
+console.log(senior)
