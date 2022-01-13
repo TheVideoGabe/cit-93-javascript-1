429c807f654d549a25ab0dda431203cbe1f9e5d4
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-01-13 14:34:29 -0800

    video 13 my code

---
+++ js-bootcamp/basics/temp-converter.js
@@ -0,0 +1,5 @@
+let fahrenheit = 32
+let celsius = (fahrenheit - 32) * 5/9
+let kelvin = celsius + 273
+let temperatures = '∘F' + fahrenheit + ' ∘C' + celsius + ' ∘K' + kelvin
+console.log(temperatures)
