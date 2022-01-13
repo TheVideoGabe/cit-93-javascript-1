c0dabc53651d76fc5f8528805301241cc6c050d3
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-01-13 14:21:54 -0800

    video 12 code

---
+++ js-bootcamp/basics/variables.js
@@ -0,0 +1,16 @@
+// rule n.1 you can't define a variable more than once.
+
+let petName = 'cosmo'
+
+petName = 'wanda'
+
+console.log(petName)
+
+// rule n.2 there are rules related to variable names
+
+let test_  = 2
+
+let result = 3 + 4
+
+// rule n.3 variable names cannot be reserved keywords.
+// let let = 12
