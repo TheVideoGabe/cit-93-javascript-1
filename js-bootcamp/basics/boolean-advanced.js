66bcf608bdce13bfe2a8a77581dcc80a74490c25
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-01-17 23:05:04 -0800

    video 16 my code

---
+++ js-bootcamp/basics/boolean-advanced.js
@@ -0,0 +1,26 @@
+// let isAccountLocked = false
+// let userRole = 'user'
+
+// // if (isAccountLocked) {
+// //     console.log('Account Is locked')
+// // } else {
+// //     console.log('Welcome')
+// // }
+
+// if (isAccountLocked) {
+//     console.log('Account Is Locked')
+// } else if (userRole === 'admin') {
+// console.log('Welcome Admin')
+// } else {
+//     console.log('Welcome')
+// }
+
+let temp = 45
+
+if (temp <= 31) {
+    console.log('Its freezing outside!')
+} else if (temp >= 120) {
+    console.log('Its too hot outside!')
+} else {
+    console.log('Its pretty nice outside!')
+}
