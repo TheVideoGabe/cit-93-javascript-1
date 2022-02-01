48ff4ea3b605e62dc2d9d598454d6dbb574a304a
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-02-01 01:22:22 -0800

    video 32 andrews solution

--- js-bootcamp/objects/objects-methods.js
+++ js-bootcamp/objects/objects-methods.js
@@ -1,3 +1,10 @@
+const showOnPage = function (text) {
+    let newParagraph = document.createElement("p");
+    newParagraph.innerHTML = text;
+    let outputDiv = document.getElementById("output");
+    outputDiv.append(newParagraph);
+  };
+
 let restaurant = {
     name: 'Grill House',
     guestCapacity: 75,
@@ -18,6 +25,32 @@ let restaurant = {
 
 restaurant.seatParty(72)
 console.log(restaurant.checkAvailability(4))
+(showOnPage(restaurant.checkAvailability(4)))
 restaurant.removeParty(5)
 console.log(restaurant.checkAvailability(4))
+(showOnPage(restaurant.checkAvailability(4)))
 
+// andrews solution
+
+let restaurant = {
+    name: 'ASB',
+    guestCapacity: 75,
+    guestCount: 0,
+    checkAvailability: function (partySize) {
+        let seatsLeft = this.guestCapacity - this.guestCount
+        return partySize <= seatsLeft
+    }, 
+    seatParty: function (partySize) {
+        this.guestCount = this.guestCount + partySize
+    },
+    removeParty: function (partySize) {
+        this.guestCount = this.guestCount - partySize
+    }
+}
+
+restaurant.seatParty(72)
+console.log(restaurant.checkAvailability(4))
+(showOnPage(restaurant.checkAvailability(4)))
+restaurant.removeParty(5)
+console.log(restaurant.checkAvailability(4))
+(showOnPage(restaurant.checkAvailability(4)))
