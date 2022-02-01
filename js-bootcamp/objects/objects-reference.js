047048d59caddf781b0adc1b95e969ed89c6a867
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-02-01 00:39:13 -0800

    video 31 andrews solution

--- js-bootcamp/objects/objects-reference.js
+++ js-bootcamp/objects/objects-reference.js
@@ -1,33 +1,33 @@
-// const showOnPage = function (text) {
-//     let newParagraph = document.createElement("p");
-//     newParagraph.innerHTML = text;
-//     let outputDiv = document.getElementById("output");
-//     outputDiv.append(newParagraph);
-//   };
+ const showOnPage = function (text) {
+     let newParagraph = document.createElement("p");
+     newParagraph.innerHTML = text;
+     let outputDiv = document.getElementById("output");
+     outputDiv.append(newParagraph);
+   };
 
-let myAccount = {
-    name: 'Gabriel',
-    expenses: 0,
-    income:0
-}
+// let myAccount = {
+    // name: 'Gabriel',
+    // expenses: 0,
+    // income:0
+// }
 
-let otherAccount = myAccount
-otherAccount.income = 1000
-otherAccount = {}
+// let otherAccount = myAccount
+// otherAccount.income = 1000
+// otherAccount = {}
 
-let addExpense = function(account, amount) {
-   // account = 1
-     account.expenses = account.expenses + amount
-    console.log(account)
-    // showOnPage(account)
-}
+// let addExpense = function(account, amount) {
+//    // account = 1
+//      account.expenses = account.expenses + amount
+//     console.log(account)
+//     // showOnPage(account)
+// }
 
-addExpense(myAccount, 2.50)
-console.log(myAccount)
-// showOnPage(myAccount)
+// addExpense(myAccount, 2.50)
+// console.log(myAccount)
+// // showOnPage(myAccount)
 
-// ** challenge **
-// add income
+// // ** challenge **
+// // add income
 // resetaccount
 // get account summary
 // account for gabriel has 900$ in income. 100$ in expenses.
@@ -68,3 +68,37 @@ expense(bankAccount, 500)
 tax(bankAccount, 0.08)
 console.log(summary(bankAccount))
 (showOnPage(summary(bankAccount)))
+
+// Andrews solution
+let myAccount = {
+    name: 'andrew mead',
+    expenses: 0,
+    income: 0
+}
+
+let addExpense = function (account, amount) {
+    account.expenses = account.expenses + amount
+}
+
+let addIncome = function (account, income) {
+    account.income = account.income + income
+}
+
+let resetAccount = function (account) {
+    account.expenses = 0
+    account.income = 0
+}
+
+let getAccountSummary = function (account) {
+    let balance = account.income - account.expenses
+    return `Account for ${account.name} has $${balance}. $${account.income} in income. $${account.expenses} in expenses.`
+}
+
+addIncome(myAccount, 2000)
+addExpense(myAccount, 2.50)
+addexpense(myAccount, 160)
+console.log(getAccountSummary(myAccount))
+showOnPage(getAccountSummary(myAccount))
+resetAccount(myAccount)
+console.log(getAccountSummary(myAccount))
+showOnPage(getAccountSummary(myAccount))
