6cc3e99955618aae816b0222c0bc91af6c4a8334
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-01-31 17:49:03 -0800

    Video 28 my code

---
+++ js-bootcamp/objects/objects-101.js
@@ -0,0 +1,22 @@
+let myBook = {
+    title: '1984',
+    author: 'George Orwell',
+    pageCount: 326
+}
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+myBook.title = `Animal Farm`
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+// challenge
+//name, age, location
+
+let person = {
+    name: 'Gabriel',
+    age: 23,
+    location: 'Fresno'
+}
+
+console.log(`My name is ${person.name} and Im ${person.age}, I also live in ${person.location}.`)
