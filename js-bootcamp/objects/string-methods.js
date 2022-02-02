b898db02b31567997ad9d0d1d1d63d0fe93785cc
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-02-01 22:46:28 -0800

    video 33 my code

---
+++ js-bootcamp/objects/string-methods.js
@@ -0,0 +1,49 @@
+const showOnPage = function (text) {
+    let newParagraph = document.createElement("p");
+    newParagraph.innerHTML = text;
+    let outputDiv = document.getElementById("output");
+    outputDiv.append(newParagraph);
+  };
+
+let name = ' Andrew Mead'
+
+// length property
+console.log(name.length)
+showOnPage(name.length)
+
+//convert to uppercase
+console.log(name.toUpperCase())
+showOnPage(name.toUpperCase())
+
+// convert to lower case
+console.log(name.toLowerCase())
+showOnPage(name.toLowerCase())
+
+// includes method 
+let password = 'abc123asdf098'
+console.log(password.includes(`password`))
+showOnPage(password.includes(`password`))
+
+// trim
+console.log(name.trim())
+showOnPage(name.trim())
+
+// challenge
+
+//length is 8 characters or more and it doesnt contiain the word password
+let passwordChallenge = function (password) {
+    if (password.length >= 8) {
+        return true
+    } else if (password.includes('password')){
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(passwordChallenge('asdp'))
+showOnPage(passwordChallenge('asdp'))
+console.log(passwordChallenge('abc123!@#$%^&'))
+showOnPage(passwordChallenge('abc123!@#$%^&'))
+console.log(passwordChallenge('asdpasdfpoijpassword'))
+showOnPage(passwordChallenge('asdpasdfpoijpassword'))
