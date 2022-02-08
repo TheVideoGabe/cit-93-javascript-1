efdcf18c75843bb067b68390b03370e7a52741a5
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-02-08 00:09:11 -0800

    video 39 my code

--- js-bootcamp/arrays/notes.js
+++ js-bootcamp/arrays/notes.js
@@ -1,15 +1,25 @@
-const showOnPage = function (text) {
-    let newParagraph = document.createElement("p");
-    newParagraph.innerHTML = text;
-    let outputDiv = document.getElementById("output");
-    outputDiv.append(newParagraph);
-  };
+// const showOnPage = function (text) {
+//     let newParagraph = document.createElement("p");
+//     newParagraph.innerHTML = text;
+//     let outputDiv = document.getElementById("output");
+//     outputDiv.append(newParagraph);
+//   };
 
 const notes = ['note 1', 'note 2', 'note 3']
 
+// console.log(notes.pop())
+// notes.push('My new note')
+
+// console.log(notes.shift())
+// console.log(notes.unshift('My first note'))
+
+// notes.splice(1, 1, 'This is the new note 2')
+
+notes[2] = 'Newer note 3'
+
 console.log(notes.length)
-console.log(notes[0])
-console.log(notes[notes.length - 2])
-showOnPage(notes.length)
-showOnPage(notes[0])
-showOnPage(notes[notes.length - 2])
+// console.log(notes[0])
+console.log(notes)
+// showOnPage(notes.length)
+// showOnPage(notes[0])
+// showOnPage(notes[notes.length - 2])
--- js-bootcamp/arrays/todo.js
+++ js-bootcamp/arrays/todo.js
@@ -1,9 +1,9 @@
-const showOnPage = function (text) {
-    let newParagraph = document.createElement("p");
-    newParagraph.innerHTML = text;
-    let outputDiv = document.getElementById("output");
-    outputDiv.append(newParagraph);
-  };
+ const showOnPage = function (text) {
+     let newParagraph = document.createElement("p");
+     newParagraph.innerHTML = text;
+     let outputDiv = document.getElementById("output");
+     outputDiv.append(newParagraph);
+   };
 
 // create an array with 5 to do's
 // you have x to do's
@@ -11,17 +11,30 @@ const showOnPage = function (text) {
 
 const toDoList = ['Wake up', 'Eat Breakfast', 'Brush My Teeth', 'Take A Shower', 'Go Excercise']
 
-console.log(toDoList[0])
-console.log(toDoList[3])
-showOnPage(toDoList[0])
-showOnPage(toDoList[3])
+// delete 3rd item
+// add a new item at the end
+// remove the first item on the list
+// splice = left position on index - right how many removed after it and insert other strings.
+toDoList.splice(2,1)
+toDoList.push('poop')
+// console.log(toDoList.shift())
+// console.log(toDoList)
+showOnPage(toDoList.shift())
+showOnPage(toDoList)
+
+
+
+// console.log(toDoList[0])
+// console.log(toDoList[3])
+// showOnPage(toDoList[0])
+// showOnPage(toDoList[3])
 
 // forgot to use length to create a x todo list.
-console.log(`I have ${toDoList.length} things to do...`)
-showOnPage(`I have ${toDoList.length} things to do...`)
+// console.log(`I have ${toDoList.length} things to do...`)
+// showOnPage(`I have ${toDoList.length} things to do...`)
 // andrews solution
-const todos = ['order cat food', 'clean kitchen', 'buy food', 'do work', 'excercise']
+// const todos = ['order cat food', 'clean kitchen', 'buy food', 'do work', 'excercise']
 
-console.log(`you have ${todos.length} todos!`)
-console.log(`todo: ${todo[0]}`)
-console.log(`todo: ${todo[todos.length - 2]}`)
+// console.log(`you have ${todos.length} todos!`)
+// console.log(`todo: ${todo[0]}`)
+// console.log(`todo: ${todo[todos.length - 2]}`)
