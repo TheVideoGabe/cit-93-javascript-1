9cef4c0e7533ddcf9e79d0f933477e1ef6b5e46c
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-02-08 01:21:25 -0800

    code video 42

--- js-bootcamp/arrays/notes.js
+++ js-bootcamp/arrays/notes.js
@@ -1,53 +1,79 @@
-//  const showOnPage = function (text) {
-//      let newParagraph = document.createElement("p");
-//      newParagraph.innerHTML = text;
-//      let outputDiv = document.getElementById("output");
-//      outputDiv.append(newParagraph);
-//    };
+// //  const showOnPage = function (text) {
+// //      let newParagraph = document.createElement("p");
+// //      newParagraph.innerHTML = text;
+// //      let outputDiv = document.getElementById("output");
+// //      outputDiv.append(newParagraph);
+// //    };
 
-const notes = ['note 1', 'note 2', 'note 3']
+// const notes = ['note 1', 'note 2', 'note 3']
 
-// console.log(notes.pop())
-// notes.push('My new note')
+// // console.log(notes.pop())
+// // notes.push('My new note')
 
-// console.log(notes.shift())
-// console.log(notes.unshift('My first note'))
+// // console.log(notes.shift())
+// // console.log(notes.unshift('My first note'))
 
-// notes.splice(1, 1, 'This is the new note 2')
+// // notes.splice(1, 1, 'This is the new note 2')
 
-notes[2] = 'Newer note 3'
+// notes[2] = 'Newer note 3'
 
-notes.forEach(function (item, index) {
-    console.log(index)
-    console.log(item)
-    // showOnPage(index)
-    // showOnPage(item)
-})
+// notes.forEach(function (item, index) {
+//     console.log(index)
+//     console.log(item)
+//     // showOnPage(index)
+//     // showOnPage(item)
+// })
 
-console.log(notes.length)
-// console.log(notes[0])
-console.log(notes)
-// showOnPage(notes)
-//  showOnPage(notes.length)
-//  showOnPage(notes[0])
-// showOnPage(notes[notes.length - 2])
+// console.log(notes.length)
+// // console.log(notes[0])
+// console.log(notes)
+// // showOnPage(notes)
+// //  showOnPage(notes.length)
+// //  showOnPage(notes[0])
+// // showOnPage(notes[notes.length - 2])
 
 
-// counting... 1
+// // counting... 1
 
-for (let count = 0; count <= 2; count ++) {
-    console.log(count)
-}
-
-// for (let count = 2; count >= 0; count --) {
+// for (let count = 0; count <= 2; count ++) {
 //     console.log(count)
 // }
 
-for (let count = 0; count < notes.length; count ++) {
-    console.log(notes[count])
-}
+// // for (let count = 2; count >= 0; count --) {
+// //     console.log(count)
+// // }
 
-for (let count = notes.length -1; count >= 0; count --) {
-    console.log(notes[count])
-}
+// for (let count = 0; count < notes.length; count ++) {
+//     console.log(notes[count])
+// }
 
+// for (let count = notes.length -1; count >= 0; count --) {
+//     console.log(notes[count])
+// }
+
+const notes = [{}, {
+    title: 'my next trip',
+    body: 'I want to go to Italy',
+}, {
+    title: 'Habbits to improve on',
+    body: 'Playing guitar more often',
+}, {
+    title: 'Amazon purchases',
+    body: 'get christmas gifts for family',
+}]
+
+// console.log(notes.length)
+// console.log(notes)
+// let someObject = {}
+// let otherObject = someObject
+// console.log(otherObject === otherObject)
+// console.log(notes.indexOf('note 2'))
+
+ console.log(notes.length)
+ console.log(notes)
+
+ const index = notes.findIndex(function (note, index) {
+     console.log(note)
+     return note.title === 'Habbits to improve on'
+ })
+ console.log(index)
