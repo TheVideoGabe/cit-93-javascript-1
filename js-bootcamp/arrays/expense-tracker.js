c2c177d6e72b788e91e93fca2d952169bb414e55
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-02-14 23:39:29 -0800

    Video 46 part 2 Andrews Solution

--- js-bootcamp/arrays/expense-tracker.js
+++ js-bootcamp/arrays/expense-tracker.js
@@ -74,3 +74,42 @@ showOnPage(account.getAccountSummary())
 // account.addExpense('Rent', 950)
 // account.addExpense('Coffee', 2)
 // console.log(account.getAccountSummary())
+
+// part 2 andrrews solution
+
+const account = {
+        name: 'Andrew Mead',
+        expenses: [],
+        addExpense: function (description, amount) {
+            this.expenses.push({
+                description: description,
+                amount: amount
+            })
+        },
+        addIncome: function (description, amount) {
+            this.income.push( {
+                description: description,
+                amount: amount
+            })
+        },
+        getAccountSummary: function () {
+            let totalExpenses = 0
+            let totalIncome = 0
+            let accountBalance = 0
+
+            this.expenses.forEach(function (expense) {
+                totalExpenses = totalExpenses + expense.amount
+            })
+
+            this.income.forEach(function (income) {
+                totalIncome = totalIncome + income.amount
+            })
+            
+            return `${this.name} has a balance of $${accountBalance}. $${totalIncome} in income. $${totalAmount} in expenses.`
+
+        }
+    }
+    
+    account.addExpense('Rent', 950)
+    account.addExpense('Coffee', 2)
+    console.log(account.getAccountSummary())
