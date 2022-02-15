9ef8b4a82395a038d5b152b0f49fcd8d52fe8b2e
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-02-14 23:33:00 -0800

    video 46 part 2 my code

--- js-bootcamp/arrays/expense-tracker.js
+++ js-bootcamp/arrays/expense-tracker.js
@@ -8,6 +8,7 @@ const showOnPage = function (text) {
 const account = {
     name: 'Gabriel Corral',
     expenses: [],
+    income: [],
     addExpense: function (description, amount) {
         this.expenses.unshift({
             description: description,
@@ -16,11 +17,22 @@ const account = {
     },
     getAccountSummary: function () {
         let totalAmount = 0
+        let totalIncome = 0
         this.expenses.forEach(function (addExpenses) {
             totalAmount = totalAmount + addExpenses.amount
         })
-        return `${this.name} has ${totalAmount} in expenses.`
-    }
+        this.income.forEach(function (getIncome) {
+            totalIncome = totalIncome + getIncome.amount
+        })
+        accountBalance = totalIncome - totalAmount
+        return `${this.name} has a balance of ${accountBalance}$. ${totalIncome}$ in income. ${totalAmount} in expenses.`
+    },
+    addIncome: function (description, amount) {
+        this.income.unshift({
+            description: description,
+            amount: amount
+        })
+    },
 }
 
 // we have expenses which include -> description, amount
@@ -28,31 +40,37 @@ const account = {
 // add expense -> description ammount
 //add acount summary -> total up all expenses(for each, add to a variable) -> Gabriel C has 1250$ in expenses.
 
+// add income array to account looks like the expenses one.
+// add income method that takes a description and an amount.
+//tweak getaccount summary
+
+// Should look like "Name has a balance of $10, $100 in income, $90 in expenses."
 
 account.addExpense('Rent', 950)
 account.addExpense('Coffee', 2)
+account.addIncome('Job', 1000)
 // console.log(account.getAccountSummary())
 showOnPage(account.getAccountSummary())
 
 // part 1 andrews solution
-const account = {
-    name: 'Andrew Mead',
-    expenses: [],
-    addExpense: function (description, amount) {
-        this.expenses.push({
-            description: description,
-            amount: amount
-        })
-    },
-    getAccountSummary: function () {
-        let totalExpenses = 0
-        this.expenses.forEach(function (expense) {
-            totalExpenses = totalExpenses + expense.amount
-        })
-        return `${this.name} has ${totalExpenses} in expenses.`
-    }
-}
+// const account = {
+//     name: 'Andrew Mead',
+//     expenses: [],
+//     addExpense: function (description, amount) {
+//         this.expenses.push({
+//             description: description,
+//             amount: amount
+//         })
+//     },
+//     getAccountSummary: function () {
+//         let totalExpenses = 0
+//         this.expenses.forEach(function (expense) {
+//             totalExpenses = totalExpenses + expense.amount
+//         })
+//         return `${this.name} has ${totalExpenses} in expenses.`
+//     }
+// }
 
-account.addExpense('Rent', 950)
-account.addExpense('Coffee', 2)
-console.log(account.getAccountSummary())
+// account.addExpense('Rent', 950)
+// account.addExpense('Coffee', 2)
+// console.log(account.getAccountSummary())
