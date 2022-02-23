e37adb372296741a46a673fc556475fe2444c62c
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-02-22 23:48:24 -0800

    Video 51 Andrews solution

--- js-bootcamp/todo-app/link.js
+++ js-bootcamp/todo-app/link.js
@@ -6,3 +6,14 @@ the.forEach(function (p) {
    }
     
 })
+
+// ANdrews solution
+
+const paragraphs = document.querySelectorAll('p')
+
+paragraphs.forEach(function (paragraph) {
+   if (paragraph.textContent.includes('the')) {
+       paragraph.remove()
+   }
+    
+})
