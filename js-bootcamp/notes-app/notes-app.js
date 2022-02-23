c77f5b976b87069c95c6b2048eb9ffce99384728
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-02-22 23:21:36 -0800

    video 50 code

--- js-bootcamp/notes-app/notes-app.js
+++ js-bootcamp/notes-app/notes-app.js
@@ -1,1 +1,13 @@
-console.log("new file")
+// Dom document object model
+
+// const p = document.querySelector('p')
+// p.remove()
+
+//Query All and remove
+const ps = document.querySelectorAll('p')
+
+ps.forEach(function (p) {
+    p.textContent = "*Censored*"
+    // console.log(p.textContent)
+    // p.remove()
+})
