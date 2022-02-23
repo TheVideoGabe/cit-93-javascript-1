7ff4052c16068c41435b9c7d8d86fb239f6e7ab5
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-02-23 01:27:45 -0800

    video 52 Andrews Solution

--- js-bootcamp/todo-app/todo-app.js
+++ js-bootcamp/todo-app/todo-app.js
@@ -31,3 +31,19 @@ todos.forEach(function (listOfItems) {
     p.textContent = listOfItems.text
     document.querySelector('body').appendChild(p)
 })
+
+// Andrews solution
+
+const incompleteTodos = todos.filter(function (todo) {
+    return !todo.completed
+})
+
+const summary = document.createElement('h2')
+summary.textContent = `You have ${incompleteTodos.length} todos left`
+document.querySelector('body').appendChild(summary)
+
+todos.forEach(function (todo) {
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    document.querySelector('body').appendChild(p)
+})
