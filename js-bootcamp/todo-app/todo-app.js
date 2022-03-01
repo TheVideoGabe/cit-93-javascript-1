855043d51a1e13d15c16e06226887e61fcc0fc31
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-03-01 00:08:47 -0800

    Video 57 andrews solution

--- js-bootcamp/todo-app/todo-app.js
+++ js-bootcamp/todo-app/todo-app.js
@@ -20,14 +20,57 @@ const todos = [{
 // 3. create a renderTodods function to render and rerender the latest filtered data
 
 // Video 57 challenge starts here
+// const filters = {
+//   textSearch: ''
+// }
+
+// const renderTodos = function (todos, filters) {
+//   const filteredTodos = todos.filter(function (todo) {
+//       return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+//     })
+//     document.querySelector('#todos').innerHTML = ''
+    
+//     const summary = document.createElement('h2')
+//     summary.textContent = `You have ${incompleteTodos.length} todos left`
+//     document.querySelector('#todos').appendChild(summary)
+    
+//     filteredTodos.forEach(function (todo) {
+//       const p = document.createElement('p')
+//       p.textContent = todo.text
+//       document.querySelector('#todos').appendChild(p)
+//     })
+//   }
+//   const incompleteTodos = filteredTodos.filter(function (todo) {
+//     return !todo.completed
+//   })
+//   // Video 57 Challenge ends here.
+
+// renderTodos(todos, filters)
+
+// document.querySelector('button').addEventListener('click', function (e) {
+//     console.log('Add a new todo... ')
+// })
+
+// document.querySelector('#new-todo').addEventListener('input', function (e) {
+//   filters.searchText = e.target.value
+//   renderTodos(todos, filters)
+// })
+
+// Video 57 Andrews solution
+
 const filters = {
-  textSearch: ''
+  searchText: ''
 }
 
 const renderTodos = function (todos, filters) {
   const filteredTodos = todos.filter(function (todo) {
       return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
     })
+
+    const incompleteTodos = todos.filter(function (todo) {
+      return !todo.completed
+    })
+
     document.querySelector('#todos').innerHTML = ''
     
     const summary = document.createElement('h2')
@@ -40,18 +83,18 @@ const renderTodos = function (todos, filters) {
       document.querySelector('#todos').appendChild(p)
     })
   }
-  const incompleteTodos = filteredTodos.filter(function (todo) {
-    return !todo.completed
-  })
-  // Video 57 Challenge ends here.
 
 renderTodos(todos, filters)
 
-document.querySelector('button').addEventListener('click', function (e) {
+document.querySelector('#add-todo').addEventListener('click', function (e) {
     console.log('Add a new todo... ')
 })
 
-document.querySelector('#new-todo').addEventListener('input', function (e) {
+document.querySelector('#new-todo-text').addEventListener('input', function (e) {
+    console.log(e.target.value)
+})
+
+document.querySelector('#search-text').addEventListener('input', function (e) {
   filters.searchText = e.target.value
   renderTodos(todos, filters)
 })
