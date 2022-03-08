e6ed37f1d55cae7b49a8919fc2addffe89850de9
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-03-08 14:57:13 -0800

    Video 63 my code

--- js-bootcamp/todo-app/todo-app.js
+++ js-bootcamp/todo-app/todo-app.js
@@ -1,30 +1,21 @@
-const todos = [
-  {
-    text: "order cat food",
-    completed: true,
-  },
-  {
-    text: "clean kitchen",
-    completed: false,
-  },
-  {
-    text: "buy food",
-    completed: true,
-  },
-  {
-    text: "do work",
-    completed: false,
-  },
-  {
-    text: "excercise",
-    completed: true,
-  },
-];
+// Chellenge Video 63
+// Delete dummy data
+// read and parse the data when the app starts up
+// stringify and write the data when the new data is added
 
-const filters = {
+
+let todos = [];
+
+let filters = {
   searchText: "",
   hideCompleted: false,
 };
+
+const todosJSON = localStorage.getItem('todos')
+
+if (todosJSON !== null) {
+    todos = JSON.parse(todosJSON)
+}
 
 const renderTodos = function (todos, filters) {
   let filteredTodos = todos.filter(function (todo) {
@@ -50,8 +41,13 @@ const renderTodos = function (todos, filters) {
   document.querySelector("#todos").appendChild(summary);
 
   filteredTodos.forEach(function (todo) {
-    const p = document.createElement("p");
-    p.textContent = todo.text;
+    const p = document.createElement('p')
+
+    if (todo.text.length > 0) {
+      p.textContent = todo.text
+  } else {
+      p.textContent = 'Unnamed Todo'
+  }
     document.querySelector("#todos").appendChild(p);
   });
 };
@@ -63,34 +59,15 @@ document.querySelector("#search-text").addEventListener("input", function (e) {
   renderTodos(todos, filters);
 });
 
-// 1. create a form with a single input for todo text
-// 2. setup a submit handler and cancel the default action so we can respond
-// 3. add a new item to the todos array with that text data (completed value of false) assume its ot completed because its being added
-// 4. rerender app
-// 5. clear input field
-
-// document.querySelector('#new-form').addEventListener('submit', function (e) {
-//   e.preventDefault()
-//   todos.push(e.target.elements.todo.value, false)
-//   renderTodos(todos, filters)
-//   e.target.elements.todo.value = ''
-// })
-
-// Video 58 Andrews Solution
 document.querySelector("#new-form").addEventListener("submit", function (e) {
   e.preventDefault();
   todos.push({
     text: e.target.elements.todo.value,
     completed: false,
   });
+  localStorage.setItem('todos', JSON.stringify(todos))
   renderTodos(todos, filters);
-  e.target.elements.todo.value = "";
 });
-
-// 1. create a checkbox and setup an event listener -> "hide completed"
-// 2. create new hidecompleted filter with a default of false
-// 3. update hidecompleted and rerender list on checkbox change
-// 4. setup rerndertodos to remove completed items
 
 document.querySelector("#hide-completed").addEventListener("change", function (e) {
     filters.hideCompleted = e.target.checked
