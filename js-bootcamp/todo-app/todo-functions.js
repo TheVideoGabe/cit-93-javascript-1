cd966b55aaae8036fc98a06dba91a80f85751412
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-03-08 17:34:57 -0800

    Video 65 Andrews solution

--- js-bootcamp/todo-app/todo-functions.js
+++ js-bootcamp/todo-app/todo-functions.js
@@ -1,3 +1,5 @@
+// Andrews solution video 65
+
 // fetch existing todos from local storage
 // get savedtodos
 const getSavedTodos = function () {
@@ -20,12 +22,19 @@ const saveTodos = function (todos) {
 // render todos
 const renderTodos = function (todos, filters) {
   let filteredTodos = todos.filter(function (todo) {
-    return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+    const searchTextMatch = todo.text
+      .toLowerCase()
+      .includes(filters.searchText.toLowerCase());
+    const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
+    return searchTextMatch && hideCompletedMatch;
   });
   const incompleteTodos = todos.filter(function (todo) {
     return !todo.completed;
   });
   document.querySelector("#todos").innerHTML = "";
+  document
+    .querySelector("#todos")
+    .appendChild(generateSummaryDOM(incompleteTodos));
   filteredTodos = filteredTodos.filter(function (todo) {
     if (filters.hideCompleted) {
       return !todo.completed;
@@ -35,21 +44,78 @@ const renderTodos = function (todos, filters) {
   });
 
   filteredTodos.forEach(function (todo) {
-    generateTodoDOM(todo)
+    generateTodoDOM(todo);
+    document.querySelector("#todos").appendChild(summary);
   });
-}
+};
 // get DOM elements for an individual note
 // generate todo Dom
 const generateTodoDOM = function (todo) {
   const p = document.createElement("p");
   p.textContent = todo.text;
   document.querySelector("#todos").appendChild(p);
-  return p
+  return p;
 };
 // get DOM elements for list summary
 // generate summaryDOM
-const summaryDom = function () {
- summary = document.createElement("h2");
+const generateSummaryDOM = function (incompleteTodos) {
+  summary = document.createElement("h2");
   summary.textContent = `You have ${incompleteTodos.length} todos left`;
-  document.querySelector("#todos").appendChild(summary);
-}
+  return summary;
+};
+
+// // fetch existing todos from local storage
+// // get savedtodos
+// const getSavedTodos = function () {
+//   const todosJSON = localStorage.getItem("todos");
+
+//   if (todosJSON !== null) {
+//     return JSON.parse(todosJSON);
+//   } else {
+//     return [];
+//   }
+// };
+
+// // save todos to local storage
+// // save todods
+// const saveTodos = function (todos) {
+//   localStorage.setItem("todos", JSON.stringify(todos));
+// };
+
+// // render app todos based on filters
+// // render todos
+// const renderTodos = function (todos, filters) {
+//   let filteredTodos = todos.filter(function (todo) {
+//     return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+//   });
+//   const incompleteTodos = todos.filter(function (todo) {
+//     return !todo.completed;
+//   });
+//   document.querySelector("#todos").innerHTML = "";
+//   filteredTodos = filteredTodos.filter(function (todo) {
+//     if (filters.hideCompleted) {
+//       return !todo.completed;
+//     } else {
+//       return true;
+//     }
+//   });
+
+//   filteredTodos.forEach(function (todo) {
+//     generateTodoDOM(todo)
+//   });
+// }
+// // get DOM elements for an individual note
+// // generate todo Dom
+// const generateTodoDOM = function (todo) {
+//   const p = document.createElement("p");
+//   p.textContent = todo.text;
+//   document.querySelector("#todos").appendChild(p);
+//   return p
+// };
+// // get DOM elements for list summary
+// // generate summaryDOM
+// const summaryDom = function () {
+//  summary = document.createElement("h2");
+//   summary.textContent = `You have ${incompleteTodos.length} todos left`;
+//   document.querySelector("#todos").appendChild(summary);
+// }
