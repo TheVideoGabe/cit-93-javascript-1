17354fa0eb3954b6f831755c576585202a65417c
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-03-15 04:36:57 -0700

    Video 76 part 2 my code

--- js-bootcamp/notes-app/notes-edit.js
+++ js-bootcamp/notes-app/notes-edit.js
@@ -1,4 +1,9 @@
+// add a dom element between the title and body inputs (span)
+// set the text value : last edited 4 hours ago
+// update value on title/body/storage change
+
 const titleElement = document.querySelector('#note-title')
+const edited = document.querySelector('#edited')
 const bodyElement = document.querySelector('#note-body')
 const removeElement = document.querySelector('#remove-note')
 const noteId = location.hash.substring(1)
@@ -13,16 +18,19 @@ if (note === undefined) {
 
 titleElement.value = note.title
 bodyElement.value = note.body
+edited.textContent = 'last edited: ' + moment(note.updatedAt).fromNow()
 
 titleElement.addEventListener('input', function (e) {
     note.title = e.target.value
     note.updatedAt = moment().valueOf()
+    edited.textContent = 'last edited: ' + moment(note.updatedAt).fromNow()
     localStorage.setItem('notes', JSON.stringify(notes))
 })
 
 bodyElement.addEventListener('input', function (e) {
     note.body = e.target.value
     note.updatedAt = moment().valueOf()
+    edited.textContent = 'last edited: ' + moment(note.updatedAt).fromNow()
     localStorage.setItem('notes', JSON.stringify(notes))
 })
 
@@ -45,5 +53,6 @@ window.addEventListener('storage', function (e) {
         
         titleElement.value = note.title
         bodyElement.value = note.body
+        edited.textContent = 'last edited: ' + moment(note.updatedAt).fromNow()
     }
 })
