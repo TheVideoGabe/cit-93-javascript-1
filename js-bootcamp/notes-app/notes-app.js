35e5bdb246f8bbf3d3f60518af8046aa3b89c176
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-03-15 02:55:22 -0700

    Video 74 my code

--- js-bootcamp/notes-app/notes-app.js
+++ js-bootcamp/notes-app/notes-app.js
@@ -33,3 +33,37 @@ window.addEventListener('storage', function (e) {
         renderNotes(notes, filters)
     }
 })
+
+//unix epic - january 1st 1970 00:00:00
+// 0 is now. 1000 milliseconds is a second.
+
+// const now = new Date()
+// const timestamp = now.getTime()
+
+// const myDate = new Date(timestamp)
+
+// console.log(myDate.getFullYear())
+
+// console.log(`year: ${now.getFullYear()}`)
+// console.log(`month: ${now.getMonth()}`)
+// console.log(`Day of the month: ${now.getDate()}`)
+// console.log(`Hour: ${now.getHours()}`)
+// console.log(`Minute: ${now.getMinutes()}`)
+// console.log(`Seconds: ${now.getSeconds()}`)
+
+// create 2 dates in the past use string for date
+// get timestamps fot both
+// find out which comes first and log the time tostring()
+
+const now = new Date('march 1 1981 0:00:00')
+const timestamp = now.getTime()
+
+const now2 = new Date('march 3 1985 0:00:00')
+const timestamp2 = now2.getTime()
+
+if (timestamp < timestamp2) {
+    console.log(now.toString())
+} else {
+    console.log(now2.toString())
+}
+
