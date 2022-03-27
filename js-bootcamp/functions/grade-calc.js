9487cdfeb758229bc0339238b26f8fd2fcc0da36
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-03-27 06:24:09 -0700

    Video 85 my code

--- js-bootcamp/functions/grade-calc.js
+++ js-bootcamp/functions/grade-calc.js
@@ -1,51 +1,30 @@
-const showOnPage = function (text) {
-    let newParagraph = document.createElement("p");
-    newParagraph.innerHTML = text;
-    let outputDiv = document.getElementById("output");
-    outputDiv.append(newParagraph);
-  };
-
-let grades = function (studentScore, maxScore) {
-    let percentage = (studentScore / maxScore) * 100
-
-    if (percentage>= 90) {
-        return `student scored an A with a ${percentage}%`
-    } else if (percentage >= 80) {
-        return `student scored a B with a ${percentage}%`
-    } else if (percentage >= 70) {
-        return `student scored a C with a ${percentage}%`
-    } else if (percentage >= 60) {
-        return `student scored a D with a ${percentage}%`
-    } else {
-        return `student scored an F with a ${percentage}%`
-    }
-}
-
-let grade = grades (90, 100)
-console.log(grade)
-showOnPage(grade)
-
-// better solution 
-
-let gradeCalc = function (score, totalScore) {
+let gradeCalc = (score, totalScore) => {
     let percent = (score / totalScore) * 100
     let letterGrade = ''
-
-    if (percent>= 90) {
-        letterGrade = 'A'
-    } else if (percent >= 80) {
-        letterGrade = 'B'
-    } else if (percent >= 70) {        
-        letterGrade = 'C'
-    } else if (percent >= 60) {
-        letterGrade = 'D'
+    if (typeof score && typeof totalScore === 'number') {
+        if (percent >= 90) {
+            letterGrade = 'A'
+        } else if (percent >= 80) {
+            letterGrade = 'B'
+        } else if (percent >= 70) {
+            letterGrade = 'C'
+        } else if (percent >= 60) {
+            letterGrade = 'D'
+        } else {
+            letterGrade = 'F'
+        }
+        return `you got a ${letterGrade} (${percent}%)!`
     } else {
-        letterGrade = 'F'
+        throw Error('Write Numbers Only')
     }
-
-    return `you got a ${letterGrade} (${percent}%)!`
 }
 
-let result = gradeCalc (5, 20)
-console.log(result)
-showOnPage(result)
+
+
+try {
+    let result = gradeCalc(15, 20)
+    console.log(result)
+} catch (e) {
+    console.log('Can Only Take Numbers')
+}
+
