fa94566717bc383c63411e5861083640527dfd40
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-04-05 02:39:22 -0700

    Video 92 Andrews Solution

--- js-bootcamp/oop/hangman.js
+++ js-bootcamp/oop/hangman.js
@@ -1,28 +1,19 @@
-// set up word instance property as an array of lower case letters
-// set up another instance property to store guessed letters
-// create a method that gives you the word puzzle back
-
-// no guesses? -> ***
-// guessed 'c', 'b', and 't'? -> c*t
-
-
 const Hangman = function (word, remainingGuesses) {
     this.word = word.toLowerCase().split('')
     this.remainingGuesses = remainingGuesses
-    this.guesses = ['e',]
+    this.guessedLeters = ['c', 'e',]
 }
 
 Hangman.prototype.getPuzzle = function () {
-    let hidden = ""
-    this.word.forEach((guess) => {
-        if (this.guesses == (guess)) {
-            hidden = hidden + guess
+    let puzzle = ""
+    this.word.forEach((letter) => {
+        if (this.guessedLeters.includes(letter) || letter === ' ') {
+            puzzle += letter
         } else {
-            hidden = hidden + "*"
+            puzzle += "*"
         }
-
     }) 
-    return hidden
+    return puzzle
 }
 
 const game1 = new Hangman('cat', 2)
