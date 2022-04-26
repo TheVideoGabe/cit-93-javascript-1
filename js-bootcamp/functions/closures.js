32a4898c634b76ea9719d86bdfbb3c62a623973f
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-04-26 04:53:52 -0700

    Video 110 Andrews Solution

--- js-bootcamp/functions/closures.js
+++ js-bootcamp/functions/closures.js
@@ -35,19 +35,12 @@ const add100 = createAdder(100)
 console.log(add100(-90))
 
 // tipper
-// create a createtipper that takes in a base tip of .15 for 15%
-// set it up to return a fucntion that takes in the bill amount
-// call create tipper to generate a few functions for different percentages.
-// use the generated functions to calculate tips and print them
 const createTipper = (baseTip) => {
-    return (billAmount) => {
-        return baseTip * billAmount
+    return (amount) => {
+        return baseTip * amount
     }
 }
-// tips 15 and 10 percent out of 100$
-const tip15 = createTipper(.15)
-const tip10 = createTipper(.10)
-console.log(tip15(100))
-// prints 15
-console.log(tip10(100))
-// prints 10
+const tip20 = createTipper(.2)
+const tip30 = createTipper(.3)
+console.log(tip20(100))
+console.log(tip30(100))
