923b62cd6ea15af5871f841075a03fbfdb12834a
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-05-03 19:39:37 -0700

    Video 115 Andrews Solution

--- js-bootcamp/hangman/app.js
+++ js-bootcamp/hangman/app.js
@@ -20,31 +20,9 @@ getPuzzle('2').then((puzzle) => {
     console.log(`error:${err}`)
 })
 
-
-//convert getcountry to use fetch and return a promise
-// make sure getcountry still resolves with the country that matches
-// change the get country usage to use catch
-
 getCountry('US').then((country) => {
     console.log(country.name)
 }).catch((err) => {
     console.log(`error:${err}`)
 })
 
-// getCountry('MX').then((country) => {
-//     console.log(country.name)
-// }, (err) => {
-// console.log(`Error: ${err}`)
-// })
-
-// fetch('http://puzzle.mead.io/puzzle', {}).then((response) => {
-//     if (response.status === 200) {
-//         return response.json()
-//     } else {
-//         throw new Error('unable to fetch the puzzle')
-//     }
-// }).then((data) => {
-//     console.log(data.puzzle)
-// }).catch((error) => {
-//     console.log(error)
-// })
