3c71db29b76a60e01aa7796eed99c984d1a935db
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-05-03 20:57:44 -0700

    Video 116 My Code Second Challenge

--- js-bootcamp/hangman/app.js
+++ js-bootcamp/hangman/app.js
@@ -20,14 +20,17 @@ getPuzzle('2').then((puzzle) => {
     console.log(`error:${err}`)
 })
 
-getCountry('US').then((country) => {
-    console.log(country.name)
-}).catch((err) => {
-    console.log(`error:${err}`)
-})
+// getCountry('US').then((country) => {
+//     console.log(country.name)
+// }).catch((err) => {
+//     console.log(`error:${err}`)
+// })
 
 getLocation().then((location) => {
     console.log(`You are currently in ${location.city} ${location.region} ${location.country}!`)
+    return getCountry(location.country)
+}).then((country) => {
+    console.log(country.name)
 }).catch((err) => {
     console.log(`Error: ${err}`)
 })
