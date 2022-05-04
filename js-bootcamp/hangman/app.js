2090e5c68d55326fbbd0308143a0681b3ebfeb1f
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-05-03 23:14:43 -0700

    Video 119 Code

--- js-bootcamp/hangman/app.js
+++ js-bootcamp/hangman/app.js
@@ -1,26 +1,40 @@
-const wordEl = document.querySelector('#word')
+const puzzleEl = document.querySelector('#word')
 const guessesEl = document.querySelector('#guesses')
-const game1 = new Hangman('car parts', 2)
+let game1
 
-wordEl.textContent = game1.Puzzle
-guessesEl.textContent = game1.statusMessage
+// wordEl.textContent = game1.puzzle
+// guessesEl.textContent = game1.statusMessage
 
 window.addEventListener('keypress', function (e) {
     const guess = e.key
     game1.makeGuess(guess)
-    wordEl.textContent = game1.puzzle
+    render()
+})
+
+const render = () => {
+    puzzleEl.textContent = game1.puzzle
     guessesEl.textContent = game1.statusMessage
-    console.log(game1.status)
-})
+    render()
+}
 
-getPuzzle('2').then((puzzle) => {
-    console.log(puzzle)
-}).catch((err) => {
-    console.log(`error:${err}`)
-})
+const startGame = async () => {
+    const puzzle = await getPuzzle('2')
+    game1 = new Hangman(puzzle, 5)
+    render()
+}
 
-getCurrentCountry().then((country) => {
-    console.log(country.name)
-}).catch((error) => {
-    console.log(error)
-})
+document.querySelector('#reset').addEventListener('click', startGame)
+
+startGame()
+
+// getPuzzle('2').then((puzzle) => {
+//     console.log(puzzle)
+// }).catch((err) => {
+//     console.log(`error:${err}`)
+// })
+
+// getCurrentCountry().then((country) => {
+//     console.log(country.name)
+// }).catch((error) => {
+//     console.log(error)
+// })
