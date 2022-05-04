b18da435e4daffdaa0b3aa7be6698e9fd63eae04
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-05-03 21:56:13 -0700

    Video 118 My Code First Challenge

--- js-bootcamp/hangman/request.js
+++ js-bootcamp/hangman/request.js
@@ -9,23 +9,27 @@ const getPuzzle = async (wordCount) => {
     }
 }
 
-const getCountry = (countryCode) => {
-    return fetch(`https://restcountries.com/v3.1/all`).then((response) => {
+// convert getcountry to an async function that uses await
+// convert getlocation to an async function that uses await
+
+const getCountry = async (countryCode) => {
+    const response = await fetch(`https://restcountries.com/v3.1/all`)
+
          if (response.status === 200) {
-             return response.json()
+             const data = await response.json()
+             return data.find((country) => country.cca2 === countryCode)
          } else {
              throw new Error('Unable to fetch country')
          }
-     }).then((data) => data.find((country) => country.cca2 === countryCode))
  }
 
- const getLocation = () => {
-     return fetch(`https://ipinfo.io/json?9fba1556259b4a`).then((response) => {
+ const getLocation = async () => {
+    const response = await fetch(`https://ipinfo.io/json?9fba1556259b4a`)
          if (response.status === 200) {
-             return response.json()
+             const data = response.json()
+             return data
          } else {
              throw new Error('Unable to fetch location')
          }
-     })
  }
 
