7aa5670183c601c29cb0d7c15aca8d795c5de515
Gabrrrl <gabrielcorralcardenas@gmail.com>
2022-05-03 21:01:38 -0700

    Video 116 Second Challenge Andrews Solution

--- js-bootcamp/hangman/app.js
+++ js-bootcamp/hangman/app.js
@@ -4,7 +4,6 @@ const game1 = new Hangman('car parts', 2)
 
 wordEl.textContent = game1.Puzzle
 guessesEl.textContent = game1.statusMessage
-console.log(game1.status)
 
 window.addEventListener('keypress', function (e) {
     const guess = e.key
@@ -20,18 +19,10 @@ getPuzzle('2').then((puzzle) => {
     console.log(`error:${err}`)
 })
 
-// getCountry('US').then((country) => {
-//     console.log(country.name)
-// }).catch((err) => {
-//     console.log(`error:${err}`)
-// })
-
 getLocation().then((location) => {
-    console.log(`You are currently in ${location.city} ${location.region} ${location.country}!`)
     return getCountry(location.country)
 }).then((country) => {
     console.log(country.name)
 }).catch((err) => {
     console.log(`Error: ${err}`)
 })
-
